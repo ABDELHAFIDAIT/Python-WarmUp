07ef771e67e686a0a5aa0f2688a28a118d0dcd68
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2025-09-16 10:16:38 +0100

    Solve Challenge 3

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 name = input("Enter your name: ")
-salary_hour = float(input("Enter your hourly salary: "))
-hours_worked = int(input("Enter the number of hours worked : "))
+try:
+    salary_hour = float(input("Enter your hourly salary: "))
+except ValueError:
+    print("Invalid input for hourly salary. Please enter a numeric value.")
+    exit(1)
+
+try :
+    hours_worked = int(input("Enter the number of hours worked : "))
+except ValueError:
+    print("Invalid input for hours worked. Please enter an integer value.")
+    exit(1)
 
 if hours_worked > 40:
     salary = (40 * salary_hour) + ((hours_worked - 40) * salary_hour * 1.5)
