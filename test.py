9f5aca8d9bbc1112936c592d2357e745e54d10bd
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2025-09-16 12:24:10 +0100

    Resolve Challenge 2 - Fonctions

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-def horaire_sup(name:str , salary_hour:float , hours_worked:int) -> float :
-    if hours_worked > 40:
-        salary = (40 * salary_hour) + ((hours_worked - 40) * salary_hour * 1.5)
-    else:
-        salary = hours_worked * salary_hour
+def calculation(N1:float , N2:float) -> tuple :
+    S = N1 + N2
+    D = N1 - N2
 
-    return salary
+    return S , D
 
 
-print(horaire_sup("Hafid" , 10 , 10))
+print(calculation(1,2))
