9afad6e48566544e85543fd3727e2d3f93a48ea7
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2025-09-16 10:01:45 +0100

    Solve Challenge 2

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-first_name = input("Enter your first name: ")
-age = input("Enter your age: ")
+name = input("Enter your name: ")
+salary_hour = float(input("Enter your hourly salary: "))
+hours_worked = int(input("Enter the number of hours worked : "))
 
-print(f"Hello {first_name}, you are {age} years old!")
+if hours_worked > 40:
+    salary = (40 * salary_hour) + ((hours_worked - 40) * salary_hour * 1.5)
+else:
+    salary = hours_worked * salary_hour
+
+print(f"Hello {name}, your salary is {salary} DH.")
